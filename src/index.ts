e159edbfc3dd4f8f9cc72b3477da6f69041dcfb3
Andy Lui <awlui2014@gmail.com>
2017-11-23 19:25:29 -0800

    pre rxjs

--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,14 @@ import { iOptions, iCollectionItem, IDictionary } from './interfaces';
 import * as io from 'socket.io-client';
 export class DataManager {
     private connection: string;
+    private authorized: boolean;
     private collections: Array<Object>;
     private isConnectionAlive: boolean;
     private connectCB?: Function;
-    private _a: SocketIOClient.Socket;
-    private Socket: WebSocket;
+    private Socket: SocketIOClient.Socket;
+    public  defaultSocket: SocketIOClient.Socket;
+    // private Socket: WebSocket;
+    private authToken: string;
     private collSockets: IDictionary<SocketIOClient.Socket> = {};
     constructor(private options: iOptions) {
         this.collections = options.collections || [];
@@ -16,11 +19,54 @@ export class DataManager {
         this.isConnectionAlive = false;
         this.init();
     }
-    init = () => {
+    public logout = () => {
+      this.authToken = null;
+    }
+    public login = (username: string, password: string) => {
+      this.collSockets['auth'].emit('authentication', {username, password});
+    }
+    private auth = () => {
+      const cs = this.collSockets;
+      let _s = io(`${this.connection}/auth`);
+      cs['auth'] = _s;
+
+      _s.on('authenticated', (e: any) => {
+      if (e) {
+        this.authToken = e.token;
+      }
+      this.authorized = true;
+      let c = this.collections;
+      c.forEach((coll: iCollectionItem) => {
+          let name = coll.name;
+          let _s = io(`${this.connection}/${name}`);
+          Object.keys(coll.subscribers).forEach((mthd: any) => {
+              _s.on(mthd, coll.subscribers[mthd]);
+          });
+
+          cs[name] = _s;
+      });
+      this.collSockets['chatrooms'].emit('joinRooms', { token: this.authToken });
+      });
+      _s.on('unauthorized', () => {
+        this.authorized = false;
+        console.log('not authed');
+      });
+      // cs['auth'].emit('authentication', {username: 'John', password: 'secret'});
+    }
+    private roomInit = () => {
+      this.collSockets['chatrooms'] = io(`${this.connection}/chatrooms`);
+      this.collSockets['chatrooms'].on('message', (m: any) => console.log(m, 'a'));
+
+    }
+    private init = () => {
+      this.roomInit();
+      this.defaultSocket = io(`${this.connection}/`);
+      this.defaultSocket.on('message', (m: any) => console.log(m, 'default socket message'))
         const cs = this.collSockets;
         let s = io(this.connection);
-        this._a = s;
-        console.log(s);
+        this.Socket = s;
+        this.auth();
+
         s.on('connect', this._connect);
         s.on('disconnect', this._disconnect);
         s.on('reconnect_attempt', this._reconnectAttempt);
@@ -28,46 +74,40 @@ export class DataManager {
         s.on('reconnect_error', this._reconnectError);
         s.on('reconnect_failed', this._reconnectFailed);
 
-        let c = this.collections;
-        c.forEach((coll: iCollectionItem) => {
-            let name = coll.name;
-            let _s = io(`${this.connection}/${name}`);
+          return this;
 
-            Object.keys(coll.subscribers).forEach((mthd: any) => {
-                _s.on(mthd, coll.subscribers[mthd]);
-            });
-            
-            cs[name] = _s;
-        });
-        return this;
     }
     pubData = (collection: string, endpoint: string, data:any, callback: Function) => {
-        if (this.isConnectionAlive) {
-            this.collSockets[collection].emit(endpoint, data);
-            console.log(data, 'data');
+      let payload = {
+        data, token: this.authToken
+      }
+        if (this.isConnectionAlive && this.authorized) {
+            this.collSockets[collection].emit(endpoint, payload);
         } else {
-            saveToLocalStorage(collection, {endpoint, data})
+            saveToLocalStorage(collection, {endpoint, payload})
         }
         callback(this.isConnectionAlive);
     }
     _connect = () => {
+
         this.isConnectionAlive = true;
-        console.log(this._a);
         this.connectCB();
     }
-    _disconnect(){
+    _disconnect = () => {
         console.log('Disconnected')
     }
-    _reconnectAttempt() {
+    _reconnectAttempt = () => {
 
     }
-    _reconnect() {
+    _reconnect = () => {
+      this.collSockets['auth'].emit('authentication', { token: this.authToken });
+      console.log('reconnected')
 
     }
-    _reconnectError() {
+    _reconnectError = () => {
 
     }
-    _reconnectFailed() {
+    _reconnectFailed = () => {
 
     }
 }
@@ -86,4 +126,3 @@ function clearCollection(collection: string) {
 function getFromLocalStorage(collection: string) {
     return JSON.parse(localStorage.getItem(collection));
 }
-
